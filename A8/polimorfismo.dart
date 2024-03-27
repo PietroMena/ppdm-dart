d9af847cba5f1ccc139f733e140a8b61bdd8e73e
PietroMena <133895281+PietroMena@users.noreply.github.com>
2024-03-27 09:17:55 -0300

    Add files via upload

---
+++ A8/polimorfismo.dart
@@ -0,0 +1,12 @@
+import "classes.dart";
+
+void main() {
+  Lobo lobo = Lobo();
+  lobo.dormir();
+
+  Leao leao = Leao();
+  leao.correr();
+
+  Cachorro cachorro = Cachorro();
+  cachorro.caminhar();
+}
